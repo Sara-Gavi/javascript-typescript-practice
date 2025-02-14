9358bb1501a3a7d9722cf9d8a53008a46b6c30ed
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-02-14 19:37:46 +0100

    Function to mark a task as completed

--- task-manager-typescript/dist/index.js
+++ task-manager-typescript/dist/index.js
@@ -1,2 +1,19 @@
 "use strict";
 console.log("Bienvenidas al gestor de tareas");
+// Array para almacenar las tareas
+let tasks = [];
+// Función para añadir uan nueva tarea
+function addTask(name) {
+    const newTask = {
+        id: tasks.length + 1,
+        name: name,
+        completed: false,
+    };
+    tasks.push(newTask);
+    console.log(`Tarea añadida: ${newTask.name}`);
+}
+//Añadir tarea de prueba
+addTask("Aprender Typescript");
+addTask("Dormir, por favor");
+// mostrar todas las tareas
+console.log("Lista de tareas:", tasks);
