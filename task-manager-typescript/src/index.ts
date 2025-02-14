dd861b9d5082239d4c093debbddc6e4a19cbed67
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-02-14 19:21:05 +0100

    Define Task interface and initialize tasks array

--- task-manager-typescript/src/index.ts
+++ task-manager-typescript/src/index.ts
@@ -1,1 +1,12 @@
 console.log("Bienvenidas al gestor de tareas");
+
+// Declarar la estructura de una tarea usando una interfaz
+
+interface Task {
+  id: number;
+  name: string;
+  completed: boolean;
+}
+
+// Array para almacenar las tareas
+let tasks: Task[] = [];
