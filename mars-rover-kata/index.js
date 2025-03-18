2626cf965f0eb7462cf5b00209afbfe0121321cd
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-03-18 13:48:34 +0100

    Create a Rover

--- mars-rover-kata/index.js
+++ mars-rover-kata/index.js
@@ -8,3 +8,7 @@ function inicializarRover(x, y, direccion, tamañoMapa, obstaculos = []) {
     obstaculos: obstaculos, // Guardamos los obstaculos (ejemplo: [{x:2, y:2}])
   };
 }
+
+// Crear un Rover en (0,0), mirando al Norte ('N'), en un mapa de 5x5 con un obstaculo en (2,2)
+
+let rover = inicializarRover(0, 0, "N", [5, 5], [{ x: 2, y: 2 }]);
